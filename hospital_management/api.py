75eb105d3c17b7180117e1c5767ad3f6b0c3d128
Harshini <harshinij2005@gmail.com>
2026-08-04 07:07:56 +0000

    Added custom whitelisted API using Query Builder, Document API and Database API

--- hospital_management/api.py
+++ hospital_management/api.py
@@ -1,12 +1,44 @@
+from frappe.query_builder import DocType
 import frappe
 
 @frappe.whitelist()
-def create_task(task_subject):
-    task = frappe.new_doc("Task")
-    task.subject = task_subject
-    task.save()
+def test_document_database_querybuilder():
 
-    return task.name
+    Appointment = DocType("Appointment")
+    Patient = DocType("Patient Data")
+
+    records = (
+        frappe.qb.from_(Appointment)
+        .join(Patient)
+        .on(Appointment.patient == Patient.name)
+        .select(
+            Appointment.name,
+            Appointment.patient,
+            Patient.first_name,
+            Patient.last_name,
+            Appointment.status
+        )
+        .limit(5)
+        .run(as_dict=True)
+    )
+
+    if records:
+
+        # Document API
+        doc = frappe.get_doc("Appointment", records[0]["name"])
+        doc.status = "Completed"
+        doc.save()
+
+        # Database API
+        for row in records:
+            frappe.db.set_value(
+                "Appointment",
+                row["name"],
+                "department",
+                "General"
+            )
+
+    return records
+
 def custom_logic(doc, method):
     frappe.msgprint("Hook executed!")
-
